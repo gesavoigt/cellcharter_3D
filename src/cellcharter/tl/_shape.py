731799d7ed359c08bfc19ae78784c6234733c40f
gesavoigt <gesa.voigt@web.de>
2025-01-03 14:34:41 +0100

    Add cc.tl.alphashape_optimize and 3D-compatibility for cc.tl.boundary

--- src/cellcharter/tl/_shape.py
+++ src/cellcharter/tl/_shape.py
@@ -15,9 +15,121 @@ from shapely import geometry
 from shapely.ops import polygonize, unary_union
 from skimage.morphology import skeletonize
 from squidpy._docs import d
+import sys
+import alphashape
+import trimesh
+import warnings
+
+def _alphashape_optimizealpha(
+    points: np.ndarray,
+    component: int,
+    max_iterations: int = 10000,
+    lower: float = None,
+    upper: float = None,
+    silent: bool = False
+):
+    """
+    Wrapper function for alphashape.optimizealpha.
+    """
+    dim = points.shape[1]
+    if points.shape[0] < (dim+1):
+        if not silent:
+            warnings.warn(f'Component {component}: not enough points to form a shape.')
+        return component, np.nan
+
+    # Set default bounds (as 1/bound to agree with alphashape definition of alpha)
+    if upper is None:
+        upper = sys.float_info.max
+    else:
+        upper = 1 / upper
+    if lower is None:
+        if dim == 2:
+            lower = 0.
+        elif dim == 3:
+            # If zero, alphashape function would return polygon instead of mesh
+            lower = sys.float_info.min
+    else:
+        lower = 1 / lower
 
 
-def _alpha_shape(coords, alpha):
+    # Run optimization
+    alpha_inv = alphashape.optimizealpha(points, max_iterations=max_iterations, lower=lower, upper=upper, silent=silent)
+
+    if dim == 2:
+        return component, alpha_inv
+    if dim == 3:
+        if alpha_inv == 0:
+            if not silent:
+                warnings.warn(f'Component {component}: no shape found, '
+                            'alpha range might be too stringent.')
+            return component, np.nan
+        else:
+            return component, alpha_inv
+
+@d.dedent
+def alphashape_optimize(
+    adata: AnnData,
+    component_key: str = 'component',
+    max_iterations: int = 10000,
+    lower: float = None,
+    upper: float = None,
+    silent: bool = False,
+) -> tuple([ dict[int, float] , dict[int, trimesh.Trimesh] ]) | tuple([ dict[int, float] , dict[int, geometry.Polygon] ]):
+    """
+    Function to optimize alpha using alphashape.optimizealpha.
+
+    Parameters
+    ----------
+    %(adata)s
+    component_key
+        Key in :attr:`anndata.AnnData.obs` where the component labels are stored.
+    max_iterations
+        Maximum number of iterations for the alpha shape optimizer.
+    lower
+        Minimum value for the alpha parameter of the alpha shape algorithm. If not specified, set to lowest possible value.
+    upper
+        Maximum value for the alpha parameter of the alpha shape algorithm. If not specified, set to highest possible value.
+    silent
+        If ``True``, suppresses function-specific warnings.
+
+    %(copy)s
+    Returns
+    -------
+    Returns a tuple with two :class:`dict`, each with the component labels as keys.
+    In the first dictionary, values are the optimal alpha, in the second dictionary, values are the corresponding shape (boundary).
+    If no shape is found, np.nan and None are returned for alpha and shape, respectively.
+    """
+    assert (adata.obsm["spatial"].shape[1] == 2) | (adata.obsm["spatial"].shape[1] == 3), "Points must be 2D or 3D."
+
+    components = [component for component in adata.obs[component_key].unique() if component != -1 and not np.isnan(component)]
+
+    alphas = {}
+    shapes = {}
+    with ProcessPoolExecutor() as executor:
+        futures = {
+            executor.submit(
+                _alphashape_optimizealpha,
+                adata.obsm["spatial"][adata.obs[component_key] == component, :],
+                component,
+                max_iterations,
+                lower,
+                upper,
+                silent
+            ): component
+            for component in components
+        }
+
+        for future in as_completed(futures):
+            component, alpha_inv = future.result()
+            alphas[component] = 1/alpha_inv
+            if not np.isnan(alpha_inv):
+                shapes[component] = alphashape.alphashape(adata.obsm["spatial"][adata.obs[component_key] == component, :], alpha_inv)
+            else:
+                shapes[component] = None
+
+    return alphas, shapes
+
+def _alpha_shape_2D(coords, alpha):
     """
     Compute the alpha shape (concave hull) of a set of points.
 
@@ -53,34 +165,67 @@ def _alpha_shape(coords, alpha):
     triangles = list(polygonize(m.geoms))
     return unary_union(triangles), triangles, edge_points
 
+def _process_component(points, component, hole_area_ratio=0.1, alpha_start=None):
+    """
+    Guide the alpha shape creation.
+    For 2D data, alpha_start is doubled until a valid shape is found
+    (min. 10 edge points, and small holes integrated based on hole_area_ratio).
+    For 3D data, if alpha_start does not provide a valid shape,
+    alpha is optimized using alphashape.optimizealpha.
+    Note that the alpha value used by the alphashape package seems to be the
+    inverse of the alpha value used by cellcharter. For clarity, we use the cellcharter
+    definition and pass the inverse to alphashape functions.
+    """
+    if points.shape[1] == 2:
+        if alpha_start is not None:
+            alpha = alpha_start
+        else:
+            alpha = 2000
+        polygon, triangles, edge_points = _alpha_shape_2D(points, alpha)
 
-def _process_component(points, component, hole_area_ratio=0.1, alpha_start=2000):
-    alpha = alpha_start
-    polygon, triangles, edge_points = _alpha_shape(points, alpha)
+        while (
+            type(polygon) is not geometry.polygon.Polygon
+            or type(polygon) is geometry.MultiPolygon
+            or edge_points.shape[0] < 10
+        ):
+            alpha *= 2
+            polygon, triangles, edge_points = _alpha_shape_2D(points, alpha)
 
-    while (
-        type(polygon) is not geometry.polygon.Polygon
-        or type(polygon) is geometry.MultiPolygon
-        or edge_points.shape[0] < 10
-    ):
-        alpha *= 2
-        polygon, triangles, edge_points = _alpha_shape(points, alpha)
+        boundary_with_holes = max(triangles, key=lambda triangle: triangle.area)
+        boundary = polygon
 
-    boundary_with_holes = max(triangles, key=lambda triangle: triangle.area)
-    boundary = polygon
+        for interior in boundary_with_holes.interiors:
+            interior_polygon = geometry.Polygon(interior)
+            hole_to_boundary_ratio = interior_polygon.area / boundary.area
+            if hole_to_boundary_ratio > hole_area_ratio:
+                try:
+                    difference = boundary.difference(interior_polygon)
+                    if isinstance(difference, geometry.Polygon):
+                        boundary = difference
+                except Exception:  # noqa: B902
+                    pass
+        return component, boundary
 
-    for interior in boundary_with_holes.interiors:
-        interior_polygon = geometry.Polygon(interior)
-        hole_to_boundary_ratio = interior_polygon.area / boundary.area
-        if hole_to_boundary_ratio > hole_area_ratio:
-            try:
-                difference = boundary.difference(interior_polygon)
-                if isinstance(difference, geometry.Polygon):
-                    boundary = difference
-            except Exception:  # noqa: B902
-                pass
-    return component, boundary
+    else: # 3D
+        if alpha_start is None:
+            alpha_start = sys.float_info.min # must not be zero
+        alpha = 1 / alpha_start
 
+        # Check if initial alpha returns valid shape
+        mesh = alphashape.alphashape(points, alpha, silent=True)
+        if isinstance(mesh, trimesh.base.Trimesh):
+            is_valid = len(mesh.faces) > 0 and mesh.is_watertight and all(
+                trimesh.proximity.signed_distance(mesh, list(points)) >= 0)
+            if is_valid:
+                return component, mesh
+
+        # Otherwise, optimize alpha
+        component, alpha = _alphashape_optimizealpha(points, component, silent=True)
+        if not np.isnan(alpha):
+            mesh = alphashape.alphashape(points, alpha)
+            return component, mesh
+        else:
+            return component, None
 
 @d.dedent
 def boundaries(
@@ -89,7 +234,7 @@ def boundaries(
     min_hole_area_ratio: float = 0.1,
     alpha_start: int = 2000,
     copy: bool = False,
-) -> None | dict[int, geometry.Polygon]:
+) -> None | dict[int, geometry.Polygon] | dict[int, trimesh.Trimesh]:
     """
     Compute the topological boundaries of sets of cells.
 
@@ -99,9 +244,10 @@ def boundaries(
     cluster_key
         Key in :attr:`anndata.AnnData.obs` where the cluster labels are stored.
     min_hole_area_ratio
-        Minimum ratio between the area of a hole and the area of the boundary.
+        Minimum ratio between the area of a hole and the area of the boundary (only relevant for 2D data).
     alpha_start
         Starting value for the alpha parameter of the alpha shape algorithm.
+
     %(copy)s
     Returns
     -------
@@ -112,6 +258,7 @@ def boundaries(
     """
     assert 0 <= min_hole_area_ratio <= 1, "min_hole_area_ratio must be between 0 and 1"
     assert alpha_start > 0, "alpha_start must be greater than 0"
+    assert (adata.obsm["spatial"].shape[1] == 2) | (adata.obsm["spatial"].shape[1] == 3), "adata.obsm['spatial'] must be of shape Nx2 or Nx3"
 
     clusters = [cluster for cluster in adata.obs[cluster_key].unique() if cluster != -1 and not np.isnan(cluster)]
 
@@ -120,7 +267,7 @@ def boundaries(
         futures = {
             executor.submit(
                 _process_component,
-                adata.obsm["spatial"][adata.obs[cluster_key] == cluster, :2],
+                adata.obsm["spatial"][adata.obs[cluster_key] == cluster, :],
                 cluster,
                 min_hole_area_ratio,
                 alpha_start,
